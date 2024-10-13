e665e98a92e2a1e3098e437791acec80086b5841
AAlves668 <akinyelealves@gmail.com>
2024-10-13 16:46:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var elementosDuvida = document.querySelectorAll('.duvida')
+    //conteudo da função
+elementosDuvida.forEach(function(duvida) {
+    duvida.addEventListener('click', function(){
+    })
+})
