6eb26590488d5a24445d58823472070d10987130
AAlves668 <akinyelealves@gmail.com>
2024-10-13 17:14:40 -0300

    Update script.js
    inclusão toogle

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ var elementosDuvida = document.querySelectorAll('.duvida')
     //conteudo da função
 elementosDuvida.forEach(function(duvida) {
     duvida.addEventListener('click', function(){
+        duvida.classList.toggle('ativa')
     })
 })
